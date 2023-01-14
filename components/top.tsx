8f4de777b82cdde1776a23eceb3e2c4e1771e583
ThaBeanBoy <gtchipoyera@gmail.com>
2023-01-14 13:54:37 +0200

    Set up tailwind

--- components/top.tsx
+++ components/top.tsx
@@ -1,23 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Top() {
   return (
     <header>
       <ul>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href='/'>Drinker</Link>
+          <Link href="/">Drinker</Link>
         </li>
         <li>
-          <Link href='/'>About Us</Link>
+          <Link href="/">About Us</Link>
         </li>
         <li>
-          <Link href='/'>Contact Us</Link>
+          <Link href="/">Contact Us</Link>
         </li>
         <li>
-          <Link href='/'>Shop</Link>
+          <Link href="/">Shop</Link>
         </li>
       </ul>
     </header>
